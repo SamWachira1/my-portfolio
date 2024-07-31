6e37a75014817f2658c0b96bd2772d5225635ae1
Samuel <187swachira@gmail.com>
2024-07-31 19:51:23 -0400

    uploading configs

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -1,4 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,6 +14,14 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      ...colors,
+      primary: colors.purple,
+      secondary: colors.pink,
+      neutral: colors.neutral,
+      gray: colors.gray,
+      slate: colors.slate,
     },
   },
   plugins: [],
